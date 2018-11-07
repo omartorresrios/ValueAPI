023e7a4ef7279315a46c55f3fec7be609193a6de
Omar Torres <torresomar44@gmail.com>
2018-11-07 14:34:16 -0500

    Add Review and User serializer

---
+++ app/serializers/review_serializer.rb
@@ -0,0 +1,6 @@
+class ReviewSerializer < ActiveModel::Serializer
+  attributes :id, :body, :created_at, :from, :to
+
+  belongs_to :sender, :class_name => 'User', :foreign_key => 'from', serializer: UserSerializer
+  belongs_to :receiver, :class_name => 'User', :foreign_key => 'to', serializer: UserSerializer
+end
